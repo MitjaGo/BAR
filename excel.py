e1708bb3a2f5657d80de066d34e09043d1fbf797
MitjaGo <65914869+MitjaGo@users.noreply.github.com>
2025-11-03 21:16:14 +0100

    Update excel.py

--- excel.py
+++ excel.py
@@ -13,7 +13,8 @@ st.set_page_config(page_title="EXPORT4PHOBS", layout="wide")
 # -------------------------------
 # PASSWORD LOGIN
 # -------------------------------
-PASSWORD = st.secrets["MY_PASSWORD"]
+PASSWORD = st.secrets.get("MY_PASSWORD", "")
+
 
 # Initialize session state
 if "authenticated" not in st.session_state:
